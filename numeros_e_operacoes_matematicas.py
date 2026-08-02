fcfba702823f714f5d0de4152850049eafd63bff
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-01 22:37:38 -0300

    add: usando type para verificar numeros inteiro positivo e negativo

---
+++ numeros_e_operacoes_matematicas.py
@@ -0,0 +1,13 @@
+# Os tipos de dados numéricos principais em Python são inteiros e de ponto flutuante. 
+# Com eles, você pode armazenar dados numéricos e realizar operações matemáticas.
+
+# Os números inteiros são números sem casas decimais, positivos ou negativos
+ 
+int_1 = 30
+int_2 = -11
+
+print(type(int_1))
+print(type(int_2))
+
+# Acima vemos que utilizando o metódo type, indica que ambos são números inteiros (integer/int)
+
