4c64817a0d9654d0757f0138fb350da1ddcc6e72
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-02 22:07:55 -0300

    add: simplifiquei adicao de dados de funcionario, pos vou transformar esses dados em uma def.

--- perfil_de_funcionario.py
+++ perfil_de_funcionario.py
@@ -4,13 +4,13 @@ nome_completo = primeiro_nome + ' ' + ultimo_nome
 
 idade_funcionario = int(input('Digite sua idade: '))
 
-#endereco = input('Informe nome da rua: ')
-#numero_endereco = int(input('nº da residência: '))
-#bairro = input('informe bairro: ')
-#municipio = input('informe município: ')
-#estado = input('Informe estado: ')
+endereco = input('Informe nome da rua: ')
+numero_endereco = int(input('nº da residência: '))
 
-empregado_info = nome_completo+', '+ str( idade_funcionario)+ ' anos'
+endereco_completo = endereco+', '
+endereco_completo +=  str(numero_endereco)
+
+empregado_info = nome_completo+' '+ str( idade_funcionario)+ ' anos.'+' '+ endereco_completo
 
 
 print(empregado_info)
