6c0b5991ad2ed5ad972560fd0e6d484be418a8d8
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-02 21:44:30 -0300

    add: variavel empregado_info que vai possuir informação completa do funcionario

--- perfil_de_funcionario.py
+++ perfil_de_funcionario.py
@@ -4,12 +4,15 @@ nome_completo = primeiro_nome + ' ' + ultimo_nome
 
 idade_funcionario = int(input('Digite sua idade: '))
 
-endereco = input('Informe nome da rua: ')
-numero_endereco = int(input('nº da residência: '))
-bairro = input('informe bairro: ')
-municipio = input('informe município: ')
-estado = input('Informe estado: ')
+#endereco = input('Informe nome da rua: ')
+#numero_endereco = int(input('nº da residência: '))
+#bairro = input('informe bairro: ')
+#municipio = input('informe município: ')
+#estado = input('Informe estado: ')
+
+empregado_info = nome_completo+', '+ str( idade_funcionario)+ ' anos'
 
 
+print(empregado_info)
 
 
