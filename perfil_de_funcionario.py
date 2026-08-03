3befbb0f47bafd94b2124287f48209b89d83a8b3
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-02 21:18:09 -0300

    feat: add variaveis: idade_funcionario, nome_completo

--- perfil_de_funcionario.py
+++ perfil_de_funcionario.py
@@ -1,2 +1,16 @@
 primeiro_nome = input('Informe primeiro nome: ')
 ultimo_nome = input('Informe último nome: ')
+nome_completo = primeiro_nome + ' ' + ultimo_nome
+
+idade_funcionario = int(input('Digite sua idade: '))
+
+endereco = input('Informe nome da rua: ')
+numero_endereco = int(input('nº da residência: '))
+bairro = input('informe bairro: ')
+municipio = input('informe município: ')
+estado = input('Informe estado: ')
+
+
+
+
+print(nome_completo)
