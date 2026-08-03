e2f38da773e5bd28608c19c75328b711db1a007b
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-03 14:50:33 -0300

    feat: add arquivo com estudo de atribuição aumentada

---
+++ augmented assignment.py
@@ -0,0 +1,17 @@
+# A atribuição aumentada combina uma operação binária com uma atribuição em uma única etapa. Ela recebe uma variável, 
+# aplica uma operação a ela com outro valor e armazena o resultado de volta na mesma variável.
+
+# A sintaxe básica de uma atribuição aumentada é a seguinte:
+
+# variable <operator>= value
+
+num1 = 26
+num1 +=20
+print(f'Cliente tem {num1} anos')
+
+# Subtração
+
+contagem = 77
+contagem -= 13
+print(f'A temperatura caiu para {contagem} graus celsius')
+
