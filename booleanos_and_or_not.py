2517aa7ec47a000681125f2923e38a66d0e7fc43
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-07 17:50:08 -0300

    add: operando OR e seus exemplos

--- booleanos_and_or_not.py
+++ booleanos_and_or_not.py
@@ -7,5 +7,27 @@ cidadao = True
 age = str(34) # transformando int para string para concatenar com a string 'Anos de idade'
 age+= ' Anos de idade'
 
-print(cidadao and age) # vai imprimir somante 34 anos de idade.
+print(cidadao and age, ('foi impresso, pois os dois operando em AND são verdadeiros')) # vai imprimir somante 34 anos de idade.
+
+''' Agora, vamos dar uma olhada no oroperador. Este operador retorna o primeiro operando se ele for verdadeiro;
+ caso contrário, retorna o segundo operando. Uma orexpressão resulta em um valor verdadeiro se 
+ pelo menos um operando for verdadeiro. O oroperador também é conhecido como operador de curto-circuito. 
+ Aqui está um exemplo: '''
+
+age = 19 
+empregado = False
+print(age or empregado, (',foi impresso pelo fato do operando OR apenas precisar de uma condição verdeira para retornar verdadeiro'))
+
+
+''' Para verificar se uma ou mais expressões são verdadeiras True, podemos
+utilizar o operando OR em uma condicional'''
+
+aprovado_prova = True
+idade = 17
+
+if idade >= 18 or aprovado_prova:
+    print('Você é elegivél para iniciar seu curso de formação na PRF')
+else:
+    print('Não elegivél para prosseguir processo no curso de formação')
+
 
