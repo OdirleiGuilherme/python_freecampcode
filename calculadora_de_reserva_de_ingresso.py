2da4869a7e0ad943e0249bc38041bcbe2b98603a
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-07 20:04:37 -0300

    feat: criada variavel para dar desconto para membros

--- calculadora_de_reserva_de_ingresso.py
+++ calculadora_de_reserva_de_ingresso.py
@@ -1,9 +1,10 @@
 preco_base = 40
+desconto = preco_base - (15/100) * 40
 tipo_assento = 'Ouro'
 horario_sessao = 'Noite'
 membro = True
 fim_de_semana = False
-desconto = 0
+
 
 idade = int(input('digite sua idade: '))
 
@@ -16,3 +17,9 @@ if idade >= 21:
     print('O usuário tem direito a ingresso para o show noturno!')
 else:
     print('Sem direito para ir ao show noturno')
+
+if membro is True:
+    desconto_membro = desconto
+    print(f'Seu ingresso saiu por R$ {desconto_membro} por ser membro')
+else:
+    print(f'O preço do igresso é R$ {preco_base}' )
