258311573096bf3f10ee142b2203f19bed8a494a
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-07 19:26:36 -0300

    feat: add  condição para reservar ingresso e condições se usuario pode participar do show .

--- calculadora_de_reserva_de_ingresso.py
+++ calculadora_de_reserva_de_ingresso.py
@@ -1,4 +1,15 @@
 preco_base = 40
-idade = 16
 tipo_assento = 'Ouro'
 horario_sessao = 'Noite'
+
+idade = int(input('digite sua idade: '))
+
+if idade > 18: 
+    print('O usuário está apto para reservar um ingresso')
+else:
+    print('Não apto para realizar reserva de ingresso')
+
+if idade >= 21: 
+    print('O usuário tem direito a ingresso para o show noturno!')
+else:
+    print('Sem direito para ir ao show noturno')
