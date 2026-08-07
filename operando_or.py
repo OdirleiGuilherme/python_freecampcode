51fb72d1d94d75d89eabb5056fe1feb4ef77bc21
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-08-07 18:06:25 -0300

    add. arquivo operandor_or.py que explica como utilizar  OR e alguns exemplos.

---
+++ operando_or.py
@@ -0,0 +1,15 @@
+# '''Operador NOT que recebe um único operando e inverte seu valor booleano.
+#  Ele converte valores verdadeiros (True) em falso (False) e valores falsos (False) em verdadeiros (True). 
+# Ao contrário dos operadores anteriores que vimos, o operador NOT inverte 
+# sempre retorna verdadeiro (True) ou falso (False). '''
+# Exemplo:
+ 
+print(not 'Hello') # retorna FALSO, pois uma string não vazia tem caractristica verdadeira
+print(not '')# Uma string vazia tem caracteristica FALSA como citada acima, mas com not ela retorna como verdadeiro
+print (not 0) # int em zero tem caracteristica FALS mas com not imprime verdadeiro
+print(not 1)
+print(not 0.0)
+print(not 3.14)
+print(not False)
+print(not True)
+
